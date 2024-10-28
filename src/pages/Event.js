16fa81e3fad16887dca95a83efd12cee8fb14a93
Sophia <sosophia10@gmail.com>
2024-10-28 17:27:02 -0400

    event pricing and date selection is now correct

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,169 +1,159 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom'; 
-import { FaBookmark } from 'react-icons/fa';  // Importing the bookmark icon
+import { useParams, Link } from 'react-router-dom';
+import { FaBookmark } from 'react-icons/fa';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { HOME_PATH } from '../App';
 
 function EventPage() {
-  const { eventName, eventDate } = useParams(); // get URL parameters
-  const [event, setEvent] = useState(null); // store the event object
-  const [loading, setLoading] = useState(true); // handle loading state
-  const [error, setError] = useState(null); // handle any fetch errors
-  const [isSaved, setIsSaved] = useState(false);  // State for saving the event
-  const [buttonText, setButtonText] = useState('Save Event');  // Track the button text
+  const { eventName, eventDate } = useParams();
+  const [event, setEvent] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [isSaved, setIsSaved] = useState(false);
+  const [buttonText, setButtonText] = useState('Save Event');
 
   useEffect(() => {
-    // Fetch the events data from the JSON file
-    fetch(`${process.env.PUBLIC_URL}/events-mock-data.json`) // Change the path accordingly
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to fetch event data. Error: ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Data fetched:' ,data); // Check the fetched data structure
-      const formattedEventName = decodeURIComponent(eventName).replace(/-/g, ' ');
-
-      const foundEvent = data.events.find(
-        (e) => e.eventName.toLowerCase() === formattedEventName.toLowerCase()
-      );
-
+    fetch(`${process.env.PUBLIC_URL}/events-mock-data.json`)
+      .then(response => response.ok ? response.json() : Promise.reject('Failed to fetch data'))
+      .then(data => {
+        const formattedEventName = decodeURIComponent(eventName).replace(/-/g, ' ');
+        const foundEvent = data.events.find(e => e.eventName.toLowerCase() === formattedEventName.toLowerCase());
 
         if (foundEvent) {
-          // Find the event detail that matches the eventDate
-          const foundEventDetail = foundEvent.eventDetails.find(
-            (detail) => detail.date === eventDate
-          );
-
-          // If we found both the event and the event detail, update the state
-          if (foundEventDetail) {
-            setEvent({
-              ...foundEvent,
-              date: foundEventDetail.date,
-              time: foundEventDetail.time,
-              ticketPrices: foundEventDetail.ticketPrices,
-              description: foundEvent.description,
-            });
-            console.log('Description:', foundEvent.description); // Log the description
-          } else {
-            setError('Event details not found for the specified date.');
-          }
+          setEvent(foundEvent);
+          setSelectedDate(foundEvent.eventDetails[0].date);  // Default to the first date available
         } else {
           setError('Event not found.');
         }
-
         setLoading(false);
       })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
+      .catch(err => {
+        console.error('Error fetching event data:', err);
         setError('Error fetching event data.');
         setLoading(false);
       });
-  }, [eventName, eventDate]);
+  }, [eventName]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  // Handle save button click
-  const handleSaveClick = () => {
-    setIsSaved(!isSaved);
-    setButtonText(isSaved ? 'Save Event' : 'Saved');  // Toggle between "Save Event" and "Saved"
+  const getPricesForDate = (date) => {
+    const eventDetail = event.eventDetails.find(detail => detail.date === date);
+    return eventDetail ? eventDetail.ticketPrices : {};
   };
 
-  // Handle mouse hover over the button
-  const handleMouseEnter = () => {
-    if (isSaved) {
-      setButtonText('Unsave');  // Change to "Unsave" when hovering over a saved event
-    }
+  const handleDateChange = (date) => {
+    setSelectedDate(date.toISOString().split('T')[0]);
   };
 
-  // Handle mouse leaving the button
-  const handleMouseLeave = () => {
-    if (isSaved) {
-      setButtonText('Saved');  // Change back to "Saved" when the mouse leaves the button
-    } else {
-      setButtonText('Save Event');  // Change back to "Save Event" when the event is not saved
-    }
+  const ticketPrices = getPricesForDate(selectedDate);
+
+  const getDateRange = (details) => {
+    const sortedDates = details.map(detail => new Date(detail.date)).sort((a, b) => a - b);
+    const startDate = sortedDates[0];
+    const endDate = sortedDates[sortedDates.length - 1];
+    return `${startDate.toDateString()} - ${endDate.toDateString()}`;
+  };
+
+  const dateRange = getDateRange(event.eventDetails);
+
+  const tileClassName = ({ date }) => {
+    const eventDates = event.eventDetails.map(detail => detail.date);
+    return eventDates.includes(date.toISOString().split('T')[0]) ? 'highlight' : null;
+  };
+
+  const tileDisabled = ({ date }) => {
+    const eventDates = event.eventDetails.map(detail => detail.date);
+    return !eventDates.includes(date.toISOString().split('T')[0]);
   };
 
   return (
     <div className="event-page">
-      
-      <h1 style={{ textTransform: 'capitalize', textAlign: 'center' }}>
-        {event.eventName.replace(/-/g, ' ')}
-      </h1>
-      <h3>Description:</h3>
-      <p>{event.description}</p>
+      <div className="event-banner" style={{ backgroundImage: 'url("/path-to-banner-image.jpg")' }}>
+        <div className="overlay">
+          <h1>{event.eventName}</h1>
+          <p>{dateRange} | {event.eventDetails[0].time}</p>
+        </div>
+      </div>
 
-      <h3>Date:</h3>
-      <p>{event.date}</p>
+      <div className="content-grid">
+        {/* Left column with "Go Back" button and event content */}
+        <div className="left-column">
+          {/* Go Back button */}
+          <div className="top-left-button">
+            <Link to={HOME_PATH} className="go-back-button">Go Back</Link>
+          </div>
 
-      <h3>Time:</h3>
-      <p>{event.time}</p>
-      <h3>Ticket Prices:</h3>
+          <div className="event-details">
+            <section className="event-info">
+              <h3>Description</h3>
+              <p>{event.description}</p>
 
-      <ul>
-        <li>Box: ${event.ticketPrices.box}</li>
-        <li>Orchestra: ${event.ticketPrices.orchestra}</li>
-        <li>Main Floor: ${event.ticketPrices.mainFloor}</li>
-        <li>Balcony: ${event.ticketPrices.balcony}</li>
-      </ul>
+              <h3>Time</h3>
+              <p>{event.eventDetails[0].time}</p>
+            </section>
 
-      <div style = {{ display: 'flex', justifyContent: 'center', gap: '20px'}}>
-      <Link to={`/Cart/${eventName}/${eventDate}`}>
-      <button 
-          style = {{
-            display: 'block',
-            padding: '15px 30px',
-            fontSize: '20px',
-            fontWeight: 'bold',
-            backgroundColor: '#FF6700',
-            color: 'white',
-            border: 'none',
-            borderRadius: '8px',
-            cursor: 'pointer'
-          }}
-        >
-          Go Back
-        </button>
-        </Link>
+            {/* Ticket Prices and Calendar in the bottom section */}
+            <div className="bottom-section">
+              <div className="calendar-section">
+                <h3>Event Calendar</h3>
+                <Calendar
+                  tileClassName={tileClassName}
+                  tileDisabled={tileDisabled}
+                  onClickDay={handleDateChange}
+                  defaultValue={
+                    new Date(Date.UTC(
+                      selectedDate.split('-')[0],
+                      selectedDate.split('-')[1] - 1,
+                      selectedDate.split('-')[2],
+                      12, 0, 0
+                    ))
+                  }
+                />
+              </div>
+              <table className="ticket-prices-table">
+                <thead>
+                  <tr>
+                    <th>Section</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.entries(ticketPrices).map(([section, price]) => (
+                    <tr key={section}>
+                      <td>{section}</td>
+                      <td>${price}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
 
-        <Link to = {`/Tickets/${eventName}/${eventDate}`}>
-        <button 
-          style = {{
-            display: 'block',
-            padding: '15px 30px',
-            fontSize: '20px',
-            fontWeight: 'bold',
-            backgroundColor: '#FF6700',
-            color: 'white',
-            border: 'none',
-            borderRadius: '8px',
-            cursor: 'pointer'
-          }}
-        >
-          Buy Tickets!
-        </button>
-        </Link>
+            </div>
+          </div>
+        </div>
 
-         {/* Save For Later button */}
-         <button
-            className="save-event-button"
-            onClick={handleSaveClick}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <FaBookmark className={`save-icon ${isSaved ? 'saved' : 'unsaved'}`} />
-            {buttonText}
-          </button>
+        {/* Right column for the button group */}
+        <div className="right-column">
+          <div className="top-right-buttons">
+          <Link to={`/tickets/${eventName}/${selectedDate}`}>
+            <button className="buy-tickets-button">Buy Tickets!</button>
+          </Link>
+
+            <button
+              className="save-event-button"
+              onClick={() => setIsSaved(!isSaved)}
+              onMouseEnter={() => setButtonText(isSaved ? 'Unsave' : 'Save Event')}
+              onMouseLeave={() => setButtonText(isSaved ? 'Saved' : 'Save Event')}
+            >
+              <FaBookmark className={`save-icon ${isSaved ? 'saved' : 'unsaved'}`} />
+              {buttonText}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-
   );
 }
 
